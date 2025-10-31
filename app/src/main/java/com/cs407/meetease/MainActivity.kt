b245ab0abe50a106a676e6249ade5360f93a6bfa
soumoe <mcao62@wisc.edu>
2025-10-31 12:07:59 -0500

    feat: add LoginScreen basic layout

--- app/src/main/java/com/cs407/meetease/MainActivity.kt
+++ app/src/main/java/com/cs407/meetease/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.cs407.meetease.ui.theme.MeetEaseTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             MeetEaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    LoginScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MeetEaseTheme {
-        Greeting("Android")
     }
 }
